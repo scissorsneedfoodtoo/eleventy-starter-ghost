b0edcb0379b8ff77ab5a4266436e2c18d27f98ad
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-06-03 18:04:03 +0900

    fix: remove dayjs config from time-ago.js

--- src/_includes/js/time-ago.js
+++ src/_includes/js/time-ago.js
@@ -2,15 +2,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const postDates = [...document.querySelectorAll('.post-card-meta > .meta-content > time.meta-item')];
 
-  // temporarily handle quirk with Ghost/Moment.js zh-cn not jiving
-  // with i18next's expected zh-CN format and simplify for the future
-  const siteLang = `{% siteLangHandler site.lang %}`
-
-  // Load dayjs plugins and set locale
-  dayjs.extend(dayjs_plugin_localizedFormat);
-  dayjs.extend(dayjs_plugin_relativeTime);
-  dayjs.locale(siteLang);
-
   postDates.forEach(date => {
     const dateStr = date.getAttribute('datetime');
 
