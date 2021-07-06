55f5a1f9b3efc554b3523c65b39dff4eeead6a55
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-07-06 16:39:14 +0900

    fix: post title escaping for social row, make js-min error more explicit

--- src/_includes/assets/js/social-row.js
+++ src/_includes/assets/js/social-row.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const title = `{{ post.title | safe }}`.replace(/'/g, '%27');
-  const twitter = `{{ post.primary_author.twitter }}`;
+  const title = encodeURIComponent('{{ post.title | escape }}');
+  const twitter = '{{ post.primary_author.twitter }}';
   const url = window.location;
   const thanks = `{% t 'social-row.default-tweet', { twitter: post.primary_author.twitter } %}.` +
       `%0A%0A${title}%0A%0A${url}`;
   const button = document.getElementById('tweet-btn');
   button.addEventListener('click', () => {
       if (twitter) {
-          window.open(`https://twitter.com/intent/tweet?text=${thanks}`, `share-twitter`, `width=550, height=235`);
+          window.open(`https://twitter.com/intent/tweet?text=${thanks}`, 'share-twitter', 'width=550, height=235');
           return false;
       }
       else {
-          window.open(`https://twitter.com/intent/tweet?text=${title}%0A%0A${url}`, `share-twitter`, `width=550, height=235`);
+          window.open(`https://twitter.com/intent/tweet?text=${title}%0A%0A${url}`, 'share-twitter', 'width=550, height=235');
           return false;
       }
   });
--- utils/transforms/js-min.js
+++ utils/transforms/js-min.js
@@ -5,7 +5,7 @@ module.exports = async (code, callback) => {
     const minified = await minify(code);
     callback(null, minified.code);
   } catch (err) {
-    console.error("Terser error: ", err);
+    console.error("Terser error: ", err, code);
     // Fail gracefully
     callback(null, code);
   }
